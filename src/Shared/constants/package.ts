feb9c0187bdd350a1e2f00cccc47544f783f9195
memothelemo <31132624+memothelemo@users.noreply.github.com>
2021-07-09 23:18:48 +0800

    VERBOSE_MODE constant fix
    Otherwise the cli will show verbose logs that no one cares reading these!

--- src/Shared/constants/package.ts
+++ src/Shared/constants/package.ts
@@ -4,4 +4,4 @@ export const PACKAGE_ROOT = path.join(__dirname, "..", "..", "..");
 export const PKG_VERSION: string = require(`../../../package.json`).version;
 
 export const VERBOSE_MODE =
-	process.env.NODE_ENV === "production" ? false : true;
+	process.env.NODE_ENV === "development" ? true : false;
